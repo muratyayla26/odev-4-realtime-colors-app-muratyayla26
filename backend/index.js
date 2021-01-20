ce3071c91b8872770addaba662d04dc3940db4d7
murat26 <74151892+muratyayla26@users.noreply.github.com>
2021-01-20 15:56:19 +0300

    unnecessary functions are deleted

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,6 @@ const http = require("http").Server(app);
 const io = require("socket.io")(http);
 const Colors = require("./clients/colors");
 
-app.get("/", (req, res) => {
-  res.end("hello socketio ilk yazi");
-});
-
 io.on("connection", (socket) => {
   console.log("a user connected");
 
@@ -17,7 +13,7 @@ io.on("connection", (socket) => {
 
   socket.on("new-color", (color) => {
     console.log(color);
-    Colors.upsert(color); //burda süslü icindeydi
+    Colors.upsert(color);
     socket.broadcast.emit("receive-color", color);
   });
 
